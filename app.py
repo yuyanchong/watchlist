e2fbb6dfe291966792180f40376e058868d6690b
yuyanchong <384788283@qq.com>
2021-08-16 16:15:10 +0800

    Add base.html

--- app.py
+++ app.py
@@ -63,8 +63,16 @@ class Movie(db.Model): # 表名将会是 movie
     title = db.Column(db.String(60)) # 电影标题
     year = db.Column(db.String(4)) # 电影年份
 
+@app.errorhandler(404)
+def page_not_found(e): # 接受异常对象作为参数
+    return render_template('404.html'), 404 # 返回模板和状态码
+
+@app.context_processor
+def inject_user(): # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user) # 需要返回字典，等同于return {'user': user}
+
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
     movies = Movie.query.all() # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
