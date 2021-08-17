835ab5969448c2bb3705c7271e2b3358e5068fc9
yuyanchong <384788283@qq.com>
2021-08-17 12:29:03 +0800

    Orignize application with package

---
+++ watchlist/__init__.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+WIN = sys.platform.startswith('win')
+if WIN: # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else: # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'
+# 注意更新这里的路径，把 app.root_path 添加到 os.path.dirname() 中
+# 以便把文件定位到项目根目录
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app) # 实例化扩展类
+
+@login_manager.user_loader
+def load_user(user_id): # 创建用户加载回调函数，接受用户 ID 作为参数
+    from watchlist.models import User
+    user = User.query.get(int(user_id)) # 用 ID 作为 User 模型的主键查询对应的用户
+    return user # 返回用户对象
+
+login_manager.login_view = 'login'
+
+@app.context_processor
+def inject_user(): # 函数名可以随意修改
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user) # 需要返回字典，等同于return {'user': user}
+
+from watchlist import views, errors, commands
